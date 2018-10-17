b90715f2690efa6fa9c4b3ea26ff13a200f1826a
ryo <ba068082@gmail.com>
2018-10-17 18:35:45 +0900

    fire storeまで完了

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -10,12 +10,19 @@
       >{{answer}}
       </v-btn>
     </div>
+    {{[audience1.length, audience2.length, audience3.length, audience4.length]}}
+    {{audience}}
   </div>
 </template>
 
 <script>
+  import {db} from '../plugins/firebase';
+
   export default {
     name: "Question",
+    data: () => ({
+       dialog: true
+    }),
     computed: {
       questionId: function () {
         return this.$route.params.question
@@ -25,6 +32,24 @@
       },
       answers: function () {
         return this.$store.state.questions[this.questionId].answers
+      },
+    },
+    firestore() {
+      return {
+        // firestoreのcommentsコレクションを参照
+        audience: db.collection('audience'),
+        audience1: db.collection('audience').where("question", "==", this.questionId).where(
+            "answer",
+            "==", "0"),
+        audience2: db.collection('audience').where("question", "==", this.questionId).where(
+            "answer",
+            "==", "1"),
+        audience3: db.collection('audience').where("question", "==", this.questionId).where(
+            "answer",
+            "==", "2"),
+        audience4: db.collection('audience').where("question", "==", this.questionId).where(
+            "answer",
+            "==", "3"),
       }
     },
   }
